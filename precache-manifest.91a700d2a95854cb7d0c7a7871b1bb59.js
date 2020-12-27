dc6ce7e62718172b6201b0e99ed39936ae44710d
NguyenCong176708 <conghoahoa1cnh@gmail.com>
2020-12-27 21:49:30 +0700

    Updates

---
+++ precache-manifest.91a700d2a95854cb7d0c7a7871b1bb59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fb97ae00d3fbe5d4a815459bf08e9df",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "c8b67dea8b1679997a88",
+    "url": "/todo-app/static/css/main.fc70e5b2.chunk.css"
+  },
+  {
+    "revision": "b3647f278ae1678d94a6",
+    "url": "/todo-app/static/js/2.1c4569c3.chunk.js"
+  },
+  {
+    "revision": "fcf7bb13763b5d6e269b43e96defdfd0",
+    "url": "/todo-app/static/js/2.1c4569c3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8b67dea8b1679997a88",
+    "url": "/todo-app/static/js/main.998fb2eb.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  },
+  {
+    "revision": "d7c94a31b868bae4a15510057601e91a",
+    "url": "/todo-app/static/media/Todo.d7c94a31.jpg"
+  }
+]);
